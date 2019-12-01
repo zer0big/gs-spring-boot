93ed35e317f50d5151cbebb064e3cefe0f9584b9
zerobig <zerobig.kim@gmail.com>
2019-12-01 15:57:11 +0900

    Updated HelloControllerTest.java

--- app/src/test/java/hello/HelloControllerTest.java
+++ app/src/test/java/hello/HelloControllerTest.java
@@ -26,6 +26,7 @@ public class HelloControllerTest {
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Hello from Zerobig's Spring Boot!")));
+                .andExpect(content().string(equalTo("Hello from Zerobig's Spring Boot on Azure DevOps!")));
     }
 }
+
